4481a7d3a9f68ce8d263e1fbc537739dc5013165
Sasha Tkachuk <tkachuk9199@gmail.com>
2024-08-05 22:07:41 +0300

    chore: added react-router-dom

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,9 +1,10 @@
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import type {Configuration as DevServerConfiguration} from 'webpack-dev-server';
 
 import type {IBuildOptions} from './types';
 
 export const buildDevServer = (options: IBuildOptions): DevServerConfiguration => ({
     port: options.port,
     open: true,
+    historyApiFallback: true,
 });
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import {Test} from './components/Test';
+import {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+
+import {MainPageAsync} from './pages/MainPage/MainPage.async';
+import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
 
 import './index.scss'
 
 export const App = () => {
     return <div className="app">
-        App
-        <Test/>
+        <Link to={'/'}>Main Page</Link>
+        <Link to={'/about'}>About Page</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route path="/" element={<MainPageAsync/>}/>
+                <Route path="/about" element={<AboutPageAsync/>}/>
+            </Routes>
+        </Suspense>
     </div>
 }
